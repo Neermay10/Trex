2c06d31d99fbbb50dc51bc0da7105609e9a68ea5
Neermay10 <74848380+Neermay10@users.noreply.github.com>
2020-11-25 11:19:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -50,7 +50,7 @@ function setup() {
   
   ground = createSprite(width/2,height-20,width,2);
   ground.addImage("ground",groundImage);
-  ground.x = ground.width /2;
+  ground.x = ground.width /1;
   ground.scale=4;
   
    gameOver = createSprite(width/2,height/2- 50);
